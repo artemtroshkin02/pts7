2c7ba9d9ce995c60fea96284fbbc0e45acb642ec
artemtroshkin02 <163731431+artemtroshkin02@users.noreply.github.com>
2024-03-17 12:25:11 +0100

    Add files via upload

---
+++ first_app.py
@@ -0,0 +1,25 @@
+import tkinter
+from tkinter import messagebox
+
+
+def button_click():
+    messagebox.showinfo(title='Информация', message='Ты жмакнул на кнопку')
+
+
+# --- Создание основного окна ---
+root = tkinter.Tk()
+root.geometry('400x250')
+
+# --- Создание заголовка ---
+root.title('Мое приложение')
+
+# --- Создание надписи ---
+label = tkinter.Label(root, text='Меня зовут Артём', font=('Arial', 16))
+label.pack()
+
+# --- Создание кнопки ---
+button = tkinter.Button(root, text='Жмакать здесь', command=button_click)
+button.pack()
+
+# --- Запуск основного цикла (отрисовка) ---
+root.mainloop()
